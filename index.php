d5cd39979daf9c8fbccb3ed42adaa615ab57ac6c
Felipe Negrelli <felipenegrelli@gmail.com>
2016-08-27 16:36:45 -0300

    Criação do header e footer e adicao do bootstrap.

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+include_once 'header.php';
+?>
+    // main content of web page must be here!
+    <div class="container">
+
+      <!-- Main component for a primary marketing message or call to action -->
+      <div class="jumbotron">
+        <h1>Navbar example</h1>
+        <p>This example is a quick exercise to illustrate how the default, static and fixed to top navbar work. It includes the responsive CSS and HTML, so it also adapts to your viewport and device.</p>
+        <p>To see the difference between static and fixed top navbars, just scroll.</p>
+        <p>
+          <a class="btn btn-lg btn-primary" href="../../components/#navbar" role="button">View navbar docs &raquo;</a>
+        </p>
+      </div>
+
+    </div> <!-- /container -->
+
+    <?php
+    echo "Teste";
+    ?>
+
+<?php
+include_once 'footer.php';
+?>
